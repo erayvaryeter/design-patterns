ee9a1f4c3d71f40c2fb60382f14e9ce8ed5f5512
Eray Varyeter <80865017+erayvaryeter@users.noreply.github.com>
2024-02-28 21:36:34 +0300

    dosyalar eklendi

---
+++ GenerateAllCacheFolders.bat
@@ -0,0 +1,2 @@
+call GenerateCache.bat Debug D:\\BUILD\\debug
+call GenerateCache.bat Release D:\\BUILD\\release
--- GenerateCache.bat
+++ GenerateCache.bat
@@ -1,32 +1,16 @@
 @echo off
 
 SET install_folder=D:\INSTALL
-IF %1==Debug GOTO Debug
-IF %1==Release GOTO Release
-GOTO End1
+set build_type=%1
+SET build_folder=%2
 
-:Debug
-SET build_folder=D:\BUILD\debug
 rmdir /s /q %install_folder%
 rmdir /s /q %build_folder%
 mkdir %install_folder%
 mkdir %build_folder%
-conan install . design-patterns/1.0.0@test/stable -s build_type=Debug --install-folder %install_folder%
+mkdir %build_folder%\.cmake\api\v1\query\client-MicrosoftVS
+xcopy query.json %build_folder%\.cmake\api\v1\query\client-MicrosoftVS
+conan install . design-patterns/1.0.0@test/stable -s build_type=%build_type% --install-folder %install_folder%
 xcopy %install_folder%\conanbuildinfo.txt %build_folder%
 xcopy %install_folder%\conanbuildinfo.cmake %build_folder%
 conan build . --install-folder %install_folder% --build-folder %build_folder% --configure
-GOTO End
-
-:Release
-SET build_folder=D:\BUILD\release
-rmdir /s /q %install_folder%
-rmdir /s /q %build_folder%
-mkdir %install_folder%
-mkdir %build_folder%
-conan install . design-patterns/1.0.0@test/stable -s build_type=Debug --install-folder %install_folder%
-xcopy %install_folder%\conanbuildinfo.txt %build_folder%
-xcopy %install_folder%\conanbuildinfo.cmake %build_folder%
-conan build . --install-folder %install_folder% --build-folder %build_folder% --configure
-GOTO End
-
-:End
